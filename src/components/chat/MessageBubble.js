e9cc9aca86ee373c7d4e213f25b1fd7bffde9cdd
OMKAR DORNALA <omkar.dornala537@gmail.com>
2026-02-01 22:33:34 +0530

    fixed error

--- src/components/chat/MessageBubble.js
+++ src/components/chat/MessageBubble.js
@@ -3,19 +3,16 @@ import userIcon from "../../assets/icons/User.svg";
 export default function MessageBubble({ text, time }) {
   return (
     <div className="message user">
-    <img src={userIcon} alt="user" className="message-user-icon" />
+      <img src={userIcon} alt="user" className="message-user-icon" />
 
-    <div className="message-content">
+      <div className="message-content">
         <span className="message-author">You</span>
-        <p className="text">{text}</p>
+        <span>{text}</span>  
       </div>
-  
 
-        <div className="message-meta">
-          <span className="message-time">{time}</span>
-        </div>
-
-     </div>
-
+      <div className="message-meta">
+        <span className="message-time">{time}</span>
+      </div>
+    </div>
   );
 }
